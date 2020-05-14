a77c20e1b8d3e73f797def4b804f5e44b4afc743
Towhid Zaman <32498111+towhid1zaman@users.noreply.github.com>
2020-05-14 16:00:42 +0600

    STL

---
+++ Basic Simulation/10038 - Jolly Jumpers.cpp
@@ -0,0 +1,65 @@
+
+// </> : towhid1zaman
+
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;typedef vector<ll> vll;
+#define fill(a) memset(a, 0, sizeof (a))
+#define all(v) (v).begin(),(v).end()
+#define sp(k) cout<<setprecision(k)<<fixed;
+#define rep(i,a) for(int i=0; i<a;i++)
+#define rep1(i,a,b) for(int i=(a);i<=(b);++i)
+#define irep(i,b,a) for(int i=(b);i>=(a);--i)
+#define minv(v) *min_element(v.begin(),v.end())
+#define maxv(v) *max_element(v.begin(),v.end())
+#define each(it,s) for(auto it = s.begin(); it != s.end(); ++it)
+#define unq(v) sort(all(v)),(v).erase(unique((v).begin(),(v).end()),(v).end())
+#define getpos(v,x) (lower_bound(v.begin(),v.end(),x)-v.begin())
+#define _ios ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define sqr(a) ((a)*(a))
+#define sz(a) int(a.size())
+#define ff first
+#define ss second
+#define pb push_back
+
+const int INF = 1e9 + 10;
+const double pi = acos(-1.0);
+const int mod = 1000000007; // (int)1e9+7
+const int maxn = 2000100;
+
+int main(){
+        _ios;
+
+        int n;
+        while(cin >> n){
+            vi a(n);
+            map<int,bool>mp;
+            a.clear();
+            rep(i,n){
+                cin >> a[i];
+                mp[a[i]] = true;
+            }
+            vi chek;
+            chek.clear();
+            rep(i,n-1){
+                chek.pb(abs(a[i] - a[i+1]));
+            }
+            int cnt = 0;
+            sort(all(chek));
+            rep(i,n-1){
+                if(chek[i]==(i+1))cnt++;
+            }
+            if(cnt==n-1)cout<<"Jolly"<<endl;
+            else cout<<"Not jolly"<<endl;
+        }
+
+return 0;
+}
+
+
+
+
+
